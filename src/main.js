27ec56be0ddeb26789bd0372988d0412a0b1d83b
Wojtek Nowaczyk <wojciech.nowaczyk@espo.eu>
2022-03-28 13:28:38 +0200

    Compund components forst exercise

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Main() {
+  return (
+    <>
+      <h1>React Exercises</h1>
+      <h2>Compound Components:</h2>
+      <Link to="/compound-components/exercise-1">
+        Compound Components Exercise 1 (Frontend Masters)
+      </Link>
+    </>
+  );
+}
+
+export default Main;
